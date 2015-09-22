dfd6f7686df65da4626485d88def84872aba5947
Thomie_Home <thomas.mueller96@bluewin.ch>
2015-09-22 23:22:41 +0200

    ölkjasdfölkjh

--- MuellerThomasMKN-151/MuellerThomasMKN-151/Controllers/GameController.cs
+++ MuellerThomasMKN-151/MuellerThomasMKN-151/Controllers/GameController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,15 +10,18 @@ namespace MuellerThomasMKN_151.Controllers
 {
     public class GameController : Controller
     {
-        private ApplicationDbContext db = ApplicationDbContext.Create();
+        protected ApplicationDbContext db = ApplicationDbContext.Create();
 
-        private Game gameData = new Game (1);
+        protected Game gameData = new Game (1);
+
         // GET: Game
         public ActionResult Index()
         {
-            gameData.completeWord = GetRandomWord();
-            gameData.showWord = CreateShowWord (gameData.completeWord);
-            ViewData ["Word"] = gameData.showWord;
+            gameData.completeWord   = GetRandomWord();
+            gameData.hiddenWord     = CreateHiddenWord (gameData.completeWord);
+            ViewData ["Word"]       = InsertSpaces(gameData.hiddenWord);
+
+            Session.Add("Hangman", gameData);
             return View();
         }
 
@@ -27,30 +31,72 @@ namespace MuellerThomasMKN_151.Controllers
             var Words = db.Words.ToList();
 
             Random rand = new Random();
-            int rnd = rand.Next (0, Words.Count()-1);
-
+            int rnd = rand.Next(0, Words.Count() - 1);
             return Words[rnd].Name;
+
         }
 
-
-        public string CreateShowWord (string completeWord)
+        // replaces every character in completeWord with a '_'
+        public string CreateHiddenWord (string completeWord)
         {
-            string showWord = "";
-            for (int i = 0; i < completeWord.Length; i++) 
-            {
-                showWord += "_ ";
-            }
-            return showWord.TrimEnd (' ');
+            return new string ('_', completeWord.Length);
         }
 
+
+        // Insert a space between every character
+        public string InsertSpaces(string word)
+        {
+            return String.Join<char>(" ", word);
+        }
 
         [HttpPost]
-        public ActionResult LetterChosen(string letter)
+        public ActionResult LetterChosen(string value)
         {
-            if (letter.Equals("A")) {
-                    return RedirectToAction("Index", "Home");
+            // error input ---- shouldn't be possible
+            if (value == null || value.Length == 0) {
+                return View();
             }
+
+            char letter = value[0];
+            // check if letter iscorrect 
+            if (IsLetterCorrect(letter)) {
+                // replace underscores in hiddenword with the letter
+                FillInLetter(letter);
+            }
+
             return View();
+        }
+
+
+        protected bool IsLetterCorrect(char letter)
+        {
+            return gameData.completeWord.Contains(letter) && !gameData.hiddenWord.Contains(letter);
+        }
+
+
+        protected void FillInLetter(char letter)
+        {
+            StringBuilder word = new StringBuilder(gameData.hiddenWord); 
+            for (int pos = pos = gameData.completeWord.IndexOf(letter); pos < gameData.completeWord.Length && pos < word.Length; pos++, pos = gameData.completeWord.IndexOf(letter, pos)) {
+                if (pos >= 0 && pos < word.Length) {
+                    word[pos] = letter;
+                }
+            }
+            gameData.hiddenWord = word.ToString();
+        }
+
+
+        protected Game.eGameState GetGameState()
+        {
+            if (gameData.currentErrors >= Game.MAX_ERRORS)
+            {
+                return Game.eGameState.LOST;
+            }
+            else if (gameData.completeWord.Equals(gameData.hiddenWord))
+            {
+                return Game.eGameState.WON;
+            }
+            return Game.eGameState.ACTIVE;
         }
 
     }
--- MuellerThomasMKN-151/MuellerThomasMKN-151/Models/Game.cs
+++ MuellerThomasMKN-151/MuellerThomasMKN-151/Models/Game.cs
@@ -6,20 +6,25 @@ using System.Threading.Tasks;
 
 namespace MuellerThomasMKN_151.Models
 {
-    class Game
+    public class Game
     {
         public static int MAX_ERRORS = 5;
+        public enum eGameState { ACTIVE, WON, LOST }
 
         public int gameID { get; set; }
         public int currentErrors { get; set; }
-        public string completeWord { get; set; }
-        public string showWord { get; set; }
+        public string completeWord { get; set; } // solution
+        public string hiddenWord { get; set; }   // Underlines for letters which aren't found yet
+
+        public eGameState state { get; set; }
+
 
         public Game (int gameID) {
             gameID          = -1;
             currentErrors   = 0;
             completeWord    = "";
-            showWord        = "";
+            hiddenWord      = "";
+            state           = eGameState.ACTIVE;
         }
     }
 }
